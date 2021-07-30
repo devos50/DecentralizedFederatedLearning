568d100174224b6ea1aee90356fa3c31cc9c5562
Martijn de Vos <mdmartijndevos@gmail.com>
2021-07-30 16:08:47 +0200

    Basic training working

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,11 +1,15 @@
 package nl.tudelft.dfl.demo
 
+import mu.KotlinLogging
 import nl.tudelft.dfl.*
 import nl.tudelft.dfl.dataset.CustomDatasetType
+import nl.tudelft.dfl.demo.nl.tudelft.dfl.EvaluationProcessor
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener
 import java.io.File
 
 fun main() {
+    val logger = KotlinLogging.logger("Application")
     val seed = 1337
     val dataset = Datasets.MNIST
     val baseDirectory = File(".")
@@ -23,7 +27,7 @@ fun main() {
             l2 = dataset.defaultL2
         ),
         TrainConfiguration(
-            maxIteration = MaxIterations.ITER_250,
+            maxIteration = MaxIterations.ITER_1000,
             gar = GARs.AVERAGE,
             communicationPattern = CommunicationPatterns.RANDOM,
             behavior = Behaviors.BENIGN,
@@ -61,13 +65,58 @@ fun main() {
     val network = MultiLayerNetwork(mlConfiguration.dataset.architecture(mlConfiguration.nnConfiguration, seed, NNConfigurationMode.REGULAR))
     network.init()
 
-//    val evaluationProcessor = EvaluationProcessor(
-//        baseDirectory,
-//        "local",
-//        ArrayList()
-//    )
-//    evaluationProcessor.newSimulation("local run", listOf(mlConfiguration), false)
-//    network.setListeners(
-//        ScoreIterationListener(printScoreIterations)
-//    )
+    val evaluationProcessor = EvaluationProcessor(
+        baseDirectory,
+        "local",
+        ArrayList()
+    )
+    evaluationProcessor.newSimulation("local run", listOf(mlConfiguration), false)
+
+    network.setListeners(
+        ScoreIterationListener(5)
+    )
+
+    val iterationsBeforeEvaluation = 5
+    var epoch = 0
+    var iterations = 0
+    var iterationsToEvaluation = 0
+    val trainConfiguration = mlConfiguration.trainConfiguration
+    epochLoop@ while (true) {
+        epoch++
+        trainDataSetIterator.reset()
+        logger.debug { "Starting epoch: $epoch" }
+        val start = System.currentTimeMillis()
+        while (true) {
+            var endEpoch = false
+            try {
+                network.fit(trainDataSetIterator.next())
+            } catch (e: NoSuchElementException) {
+                endEpoch = true
+            }
+            iterations += 1
+            iterationsToEvaluation += 1
+            logger.debug { "Iteration: $iterations" }
+
+            if (iterationsToEvaluation >= iterationsBeforeEvaluation) {
+                iterationsToEvaluation = 0
+                val end = System.currentTimeMillis()
+                evaluationProcessor.evaluate(
+                    testDataSetIterator,
+                    network,
+                    mapOf(),
+                    end - start,
+                    iterations,
+                    epoch,
+                    0,
+                    false
+                )
+            }
+            if (iterations >= trainConfiguration.maxIteration.value) {
+                break@epochLoop
+            }
+            if (endEpoch) {
+                break
+            }
+        }
+    }
 }
---
+++ src/main/kotlin/nl/tudelft/dfl/EvaluationProcessor.kt
@@ -0,0 +1,271 @@
+package nl.tudelft.dfl.demo.nl.tudelft.dfl
+
+import mu.KotlinLogging
+import nl.tudelft.dfl.MLConfiguration
+import nl.tudelft.dfl.d
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
+import org.nd4j.evaluation.IEvaluation
+import org.nd4j.evaluation.classification.Evaluation
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator
+import java.io.File
+import java.io.PrintWriter
+import java.text.SimpleDateFormat
+import java.util.*
+
+private const val DATE_PATTERN = "yyyy-MM-dd_HH.mm.ss"
+private val DATE_FORMAT = SimpleDateFormat(DATE_PATTERN, Locale.US)
+private val logger = KotlinLogging.logger("EvaluationProcessor")
+
+class EvaluationProcessor(
+    baseDirectory: File,
+    runner: String,
+    private val extraElementNames: List<String>
+) {
+    private val configurationHeader = arrayOf(
+        "name",
+        "simulationIndex",
+        "transfer",
+        "dataset",
+        "optimizer",
+        "learning rate",
+        "momentum",
+        "l2",
+
+        "batchSize",
+        "iteratorDistribution",
+        "maxTestSamples",
+
+        "gar",
+        "communicationPattern",
+        "behavior",
+        "numEpochs",
+        "slowdown",
+        "joiningLate",
+        "iterationsBeforeEvaluation",
+        "iterationsBeforeSending",
+
+        "local model poisoning attack",
+        "#attackers"
+    ).joinToString(",")
+
+    @Transient
+    private val configurationLines = arrayListOf(configurationHeader)
+
+    private val evaluationHeader = arrayOf(
+        "environment",
+        "simulationIndex",
+        "elapsedTime",
+        "epoch",
+        "iteration",
+        "accuracy",
+        "f1",
+        "precision",
+        "recall",
+        "gmeasure",
+        "mcc",
+        "score",
+        "before or after averaging",
+        "#peers included in current batch"
+    )
+
+    @Transient
+    private val evaluationLines = arrayListOf(evaluationHeader)
+    private val fileDirectory = File(baseDirectory.path, "evaluations")
+    private val fileResults = File(fileDirectory, "evaluation-$runner-${DATE_FORMAT.format(Date())}.csv")
+    private var fileMeta = File(fileDirectory, "evaluation-$runner-${DATE_FORMAT.format(Date())}.meta.csv")
+    private lateinit var currentName: String
+    private val startTime: Long
+
+    init {
+        if (!fileDirectory.exists()) {
+            fileDirectory.mkdirs()
+        }
+        fileResults.createNewFile()
+        fileMeta.createNewFile()
+
+        if (fileLog == null) {
+            fileLog = File(fileDirectory, "evaluation-${DATE_FORMAT.format(Date())}.txt")
+            fileLog!!.createNewFile()
+        }
+
+        val newEvaluationHeader = Array(evaluationHeader.size + extraElementNames.size) { "" }
+        evaluationHeader.copyInto(newEvaluationHeader)
+        for ((index, name) in extraElementNames.withIndex()) {
+            newEvaluationHeader[evaluationHeader.size + index] = name
+        }
+        evaluationLines[0] = newEvaluationHeader
+        startTime = System.currentTimeMillis()
+    }
+
+    private fun convertToCSV(data: Array<String>): String {
+        return data.joinToString(", ") { escapeSpecialCharacters(it) }
+    }
+
+    private fun escapeSpecialCharacters(data: String): String {
+        return if (data.contains(",") || data.contains("\"") || data.contains("'")) {
+            "\"" + data.replace("\"", "\"\"") + "\""
+        } else {
+            data.replace("\\R".toRegex(), " ")
+        }
+    }
+
+    fun newSimulation(
+        name: String,
+        mlConfiguration: List<MLConfiguration>,
+        transfer: Boolean
+    ) {
+        this.currentName = name
+        mlConfiguration.forEachIndexed { index, configuration ->
+            val line = parseConfiguration(name, index, configuration, transfer)
+            configurationLines.add(line)
+        }
+        PrintWriter(fileMeta).use { pw -> configurationLines.forEach(pw::println) }
+    }
+
+    private fun parseConfiguration(name: String, index: Int, configuration: MLConfiguration, transfer: Boolean): String {
+        val nnConfiguration = configuration.nnConfiguration
+        val datasetIteratorConfiguration = configuration.datasetIteratorConfiguration
+        val trainConfiguration = configuration.trainConfiguration
+        val modelPoisoningConfiguration = configuration.modelPoisoningConfiguration
+        return arrayOf(
+            name,
+            index.toString(),
+            transfer,
+            configuration.dataset.text,
+
+            nnConfiguration.optimizer.text,
+            nnConfiguration.learningRate.text,
+            nnConfiguration.momentum?.text ?: "<null>",
+            nnConfiguration.l2.text,
+
+            datasetIteratorConfiguration.batchSize.text,
+            datasetIteratorConfiguration.distribution.toString().replace(", ", "-"),
+            datasetIteratorConfiguration.maxTestSamples.text,
+
+            trainConfiguration.gar.text,
+            trainConfiguration.communicationPattern.text,
+            trainConfiguration.behavior.text,
+            trainConfiguration.maxIteration.text,
+            trainConfiguration.slowdown.text,
+            trainConfiguration.joiningLate.text,
+            trainConfiguration.iterationsBeforeEvaluation,
+            trainConfiguration.iterationsBeforeSending,
+
+            modelPoisoningConfiguration.attack.text,
+            modelPoisoningConfiguration.numAttackers.text,
+        ).joinToString(",")
+    }
+
+    private fun call(
+        evaluations: Array<out IEvaluation<*>>,
+        simulationIndex: Int,
+        score: Double,
+        extraElements: Map<String, String>,
+        elapsedTime: Long,
+        iterations: Int,
+        epoch: Int
+    ): String {
+        val accuracy = evaluations[0].getValue(Evaluation.Metric.ACCURACY)
+        val f1 = evaluations[0].getValue(Evaluation.Metric.F1)
+        val precision = evaluations[0].getValue(Evaluation.Metric.PRECISION)
+        val recall = evaluations[0].getValue(Evaluation.Metric.RECALL)
+        val gMeasure = evaluations[0].getValue(Evaluation.Metric.GMEASURE)
+        val mcc = evaluations[0].getValue(Evaluation.Metric.MCC)
+        val dataLineElements = arrayOf(
+            this.currentName,
+            simulationIndex.toString(),
+            elapsedTime.toString(),
+            epoch.toString(),
+            iterations.toString(),
+            accuracy.toString(),
+            f1.toString(),
+            precision.toString(),
+            recall.toString(),
+            gMeasure.toString(),
+            mcc.toString(),
+            score.toString()
+        )
+        evaluationLines.add(Array(dataLineElements.size + extraElements.size) { "" })
+        System.arraycopy(dataLineElements, 0, evaluationLines.last(), 0, dataLineElements.size)
+        for ((name, value) in extraElements) {
+            evaluationLines.last()[dataLineElements.size + extraElementNames.indexOf(name)] = value
+        }
+        PrintWriter(fileResults).use { pw ->
+            evaluationLines
+                .map(::convertToCSV)
+                .forEach(pw::println)
+        }
+        return convertToCSV(evaluationLines.last())
+    }
+
+    fun error(e: Exception) {
+        synchronized(evaluationLines) {
+            evaluationLines.add(arrayOf(e.stackTraceToString()))
+
+            PrintWriter(fileResults).use { pw ->
+                evaluationLines
+                    .map(::convertToCSV)
+                    .forEach(pw::println)
+            }
+        }
+    }
+
+    fun done() {
+        synchronized(evaluationLines) {
+            val totalTime = System.currentTimeMillis() - startTime
+            evaluationLines.add(Array(evaluationLines[0].size) { if (it == 0) { totalTime.toString() } else "DONE" })
+
+            PrintWriter(fileResults).use { pw ->
+                evaluationLines
+                    .map(::convertToCSV)
+                    .forEach(pw::println)
+            }
+        }
+    }
+
+    fun evaluate(
+        testDataSetIterator: DataSetIterator,
+        network: MultiLayerNetwork,
+        extraElements: Map<String, String>,
+        elapsedTime: Long,
+        iterations: Int,
+        epoch: Int,
+        simulationIndex: Int,
+        logging: Boolean
+    ): String {
+        testDataSetIterator.reset()
+        val evaluations = arrayOf(Evaluation())
+
+        logger.d(logging) {
+            "Starting evaluation, #iterations = $iterations, ${
+                extraElements.getOrDefault(
+                    "before or after averaging",
+                    ""
+                )
+            }"
+        }
+        network.doEvaluation(testDataSetIterator, *evaluations)
+
+        for (evaluation in evaluations) {
+            logger.d(logging) { "${evaluation.javaClass.simpleName}:\n${evaluation.stats(false, true)}" }
+        }
+        return call(evaluations, simulationIndex, network.score(), extraElements, elapsedTime, iterations, epoch)
+    }
+
+    companion object {
+        var fileLog : File? = null
+        private val logLines = arrayListOf<String>()
+
+        fun log(message: String) {
+            synchronized(logLines) {
+                logLines.add(message)
+                if (fileLog != null) {
+                    PrintWriter(fileLog!!).use { pw ->
+                        logLines
+                            .forEach(pw::println)
+                    }
+                }
+            }
+        }
+    }
+}
